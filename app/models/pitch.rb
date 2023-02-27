9dd0ac5957eae06d9c6d5568c4bc41efd25d022e
Pedro Romariz <pedroromariz1@gmail.com>
2023-02-27 17:57:51 +0000

    models validations available field presence true

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -1,3 +1,8 @@
 class Pitch < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
+  validates :type, presence: true, inclusion: { in: %w[Basketball Padel Tenis Football Futsal Field-Hockey Hockey Volleyball Handball] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :pitch
+
+  valitades :rating, presence: true, numericality: { only_integer: true }, length: { in: 0..5 }
+  validates :comment, presence: true, length: { minimum: 3 }
 end
