990428885e2ce693d1ead214ec455f124d297895
rui silva <ruimorimsilva@gmail.com>
2023-02-28 11:15:27 +0000

    pitches controller commit v1

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,4 +1,5 @@
 class PitchesController < ApplicationController
+  before_action :set_pitch, only: [:show, :edit, :update, :destroy]
 
   def index
     @pitches = Pitch.all
@@ -15,14 +16,36 @@ class PitchesController < ApplicationController
   end
 
   def create
-    
+    @pitch = Pitch.new(pitch_params)
+    @pitch.list = @pitch
+
+    if @pitch.save
+      redirect_to pitch_path(@pitch), notice: 'Pitch created'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @pitch.update(pitch_params)
+      redirect_to pitch_path(@pitch)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @pitch.destroy
+    redirect_to pitch_path(@pitch.list), status: :see_other
   end
 
+  private
 
+  def pitch_params
+    params.require(:pitch).permit(:name, :location, :price, :type)
+  end
+
+  def set_pitch
+    @pitch = Pitch.find(params[:pitch_id])
+  end
 end
