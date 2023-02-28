09bb454780a63dc9c8433e0c17e2ed141b398969
Pedro Romariz <pedroromariz1@gmail.com>
2023-02-28 17:35:26 +0000

    added edit new _form changed to correct home route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "pages#home"
+  root to: "pitches#index"
   resources :pitches do
     resources :bookings, only: [:new, :create]
   end
