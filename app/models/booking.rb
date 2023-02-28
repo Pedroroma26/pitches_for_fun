fd0bfc38ef7f7debb69b31d0c1574bdd615ac862
Pedro Romariz <pedroromariz1@gmail.com>
2023-02-28 15:12:54 +0000

    seeds all done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :pitch
   belongs_to :user
+  has_many :reviews, dependent: :destroy
 
   validates :start_time, presence: true, comparison: { less_than: :end_time }
 end
