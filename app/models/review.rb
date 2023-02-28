fc3d0e7ca02e3a69ba91a53a39f4f24fa3e6b4ac
rui silva <ruimorimsilva@gmail.com>
2023-02-28 17:28:45 +0000

    rating implemented on the index

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  has_one :pitch, through: :bookings
 
   validates :rating, presence: true, numericality: { only_integer: true }, length: { in: 0..5 }
   validates :comment, presence: true, length: { minimum: 3 }
