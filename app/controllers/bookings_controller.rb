7ce55d8e4bb7079d94f7a31a1bf3faa10b0705d1
Pedro Romariz <pedroromariz1@gmail.com>
2023-03-01 12:42:33 +0000

    destroy method updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -33,7 +33,7 @@ class BookingsController < ApplicationController
 
   def destroy
     @booking = Booking.find(params[:id])
-    @bookmark.destroy
+    @booking.destroy
     redirect_to bookings_path, status: :see_other
   end
 
